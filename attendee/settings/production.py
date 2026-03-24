2a4498644af6e26883d06f80069ee3fd1673afc9
FunildeVendas <58042173+FunildeVendas@users.noreply.github.com>
2026-03-23 22:56:48 -0300

    Update production.py

--- attendee/settings/production.py
+++ attendee/settings/production.py
@@ -41,7 +41,7 @@ else:
     CSRF_COOKIE_SECURE = False
 
 if os.getenv("DISABLE_EMAIL", "false") != "true":
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.mailgun.org")
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
